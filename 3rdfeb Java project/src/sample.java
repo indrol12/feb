38cf93c748f829f28584636eb90259bc1cf81759
suyash <suyashgit24@gmail.com>
2025-02-03 18:19:17 +0530

    commited

--- 3rdfeb Java project/src/sample.java
+++ 3rdfeb Java project/src/sample.java
@@ -4,6 +4,7 @@ public class sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("java is popular programming language");
+System.out.println("hello this is pratiksha mam porject where we atre studying the testing like jeera");
 	}
 
 }
